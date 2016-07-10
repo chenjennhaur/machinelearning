51747dc70e9a7f7553aefd0edb50de533c94a638
chenjennhaur@hotmail.com <chenjennhaur@hotmail.com>
2016-07-10 21:13:15 +0800

    added training by store

--- kaggle/rossmann/train.py
+++ kaggle/rossmann/train.py
@@ -72,7 +72,7 @@ def set_cv(data,best_features):
 	data.loc[(data['Date']>='01-Aug-2014')&(data['Date']<='17-Sep-2014'),'CV'] = 0
 	data.loc[(data['Date']>='01-Aug-2013')&(data['Date']<='17-Sep-2013'),'CV'] = 1
 	data.loc[(data['Date']>='01-Jun-2015')&(data['Date']<='17-Jul-2015'),'CV'] = 2
-	X = data[data['Set']>0].iloc[:,best_features].values
+	X = data[data['Set']>0].loc[:,best_features].values
 	y = data[data['Set']>0].iloc[:,6].values
 	cv_set = data[data['Set']>0].iloc[:,41].values
 	ps = PredefinedSplit(test_fold=cv_set)
@@ -80,12 +80,12 @@ def set_cv(data,best_features):
 
 def parameter_tune(data,features,X,y,ps):
 	param_grid = {
-              "max_features": [5,10],
-              "min_samples_split": [10],
-              "min_samples_leaf": [10]
+              "max_features": [5,20],
+              "min_samples_split": [5,10],
+              "min_samples_leaf": [5,10]
              }
 			 
-	model = RandomForestRegressor(n_jobs=-1)
+	model = RandomForestRegressor()
 	rsearch = GridSearchCV(estimator=model,param_grid=param_grid,scoring=rmspe_scorer,cv=ps)
 	rsearch.fit(X,y)
 	
@@ -129,6 +129,39 @@ def select_features(model,data,ps):
 	print("Minimum Score with features ",min_score)
 	print("Best Features ",curr_features)
 	return list(curr_features)
+
+def onemodelperstore(data,best_features):
+	cv = 2
+	modelall = {}
+	ypredict = []
+	yactual = []
+	# X_tr = data.loc[(data['CV']!=0,best_features]
+	# y_tr = data.loc[(data['CV']!=0,best_features]
+	for i in range(1,1115):
+		#Training
+		model = RandomForestRegressor()
+		X_tr = data[(data['Set']>0)&(data['Store']==i)&(data['CV']!=cv)].loc[:,best_features].values
+		y_tr = data[(data['Set']>0) &(data['Store']==i)&(data['CV']!=cv)].iloc[:,6].values
+		model.fit(X_tr,y_tr)
+		modelall[i] = model
+		#Cross Validation
+		X_cv = data.loc[(data['CV']==cv)&(data['Set']>0)&(data['Store']==i),best_features].values
+		y_cv = data[(data['CV']==cv)&(data['Set']>0)&(data['Store']==i)].iloc[:,6].values
+		y_pred = model.predict(X_cv)
+		print("y_predict,y_actual")
+		print(y_pred.shape," ",y_cv.shape)
+		# print("y_predict",y_pred)
+		# print("y_actual",y_cv)
+		ypredict = np.hstack((ypredict,y_pred))
+		yactual = np.hstack((yactual,y_cv)) 
+		print("ypredict,yactual")
+		print(ypredict.shape," ",yactual.shape)
+		# print("y_predict_all",ypredict)
+		# print("y_actual_all",yactual)
+	
+	print("Cross Validation", rmspe(yactual,ypredict))
+	return modelall
+	
 	
 	
 rmspe_scorer = make_scorer(rmspe,greater_is_better=False)
@@ -136,7 +169,8 @@ rmspe_scorer = make_scorer(rmspe,greater_is_better=False)
 dfxgb = joblib.load('pkl/dfxgb.pkl')
 # pred_y = joblib.load('pkl/pred_y.pkl')
 
-best_features = (10,2,4,5,7,9,13,14,15,17,18,19,32,33,34,35,36,37,38,39,40)
+# best_features = (10,2,4,5,7,9,13,14,15,17,18,19,32,33,34,35,36,37,38,39,40)
+best_features = ['Promo','CompetitionOpenSinceYear','CompetitionFlag','DateYear','DayOfWeek','stype_2','stype_3','CompetitionDistance','Store']
 # Best
 # (2,4,5,7,9,11,12,13,18,19,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40) - #0.133288201516
 # (2,4,5,7,9,11,12,13,14,15,17,28,29,30,31,32,33,34,35,36,37,38,39,40) - #0.161455
@@ -148,10 +182,10 @@ best_features = (10,2,4,5,7,9,13,14,15,17,18,19,32,33,34,35,36,37,38,39,40)
 X,y,ps = set_cv(dfxgb,best_features)
 # rf = RandomForestRegressor(n_jobs=-1)
 # best_features = select_features(rf,dfxgb,ps)	
-# best_features = ['Promo','CompetitionOpenSinceYear','CompetitionFlag','DateYear','DayOfWeek','stype_2','stype_3','CompetitionDistance','Store']
 clf = parameter_tune(dfxgb,best_features,X,y,ps)
 
-
+# 1 model for each store
+clf_store = onemodelperstore(dfxgb,best_features)
 
 # Submission
 # labels = dfxgb[dfxgb['Set']==0].iloc[:,3].values
